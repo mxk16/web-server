182b9478e80eb381955f481be2afb45c21a07558
Maxim Kloushin <makr1975@yandex.ru>
2016-12-20 17:33:43 +0100

    Refactor middleware

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-var express = require ('express');
+// var express = require ('express');
+// var app = express();
+// var PORT = 3000;
+
+// var middleware = require('./middleware.js');
+
+// app.use(middleware.logger);
+// //app.use(middleware.requireAuthentication);
+
+// app.get('/about', middleware.requireAuthentication, function(req, res){
+// 	res.send('About Us!!');
+// });
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.listen(PORT, function(){
+// 	console.log('Express server started on port ' + PORT + '!')
+// });
+
+var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, res, next){
-		console.log('Request ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-//app.use(middleware.requireAuthentication);
 
-app.get('/about', middleware.requireAuthentication, function(req, res){
-	res.send('About Us!!');
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function(){
-	console.log('Express server started on port ' + PORT + '!')
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
 
 
